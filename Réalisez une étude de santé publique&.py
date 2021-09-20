6dafb5bd0b0e9c4a48f89383c8cf7fa9f79e4047
Rowaida KANAAN <73246721+ROWAIDAK@users.noreply.github.com>
2021-09-20 20:50:07 +0200

    Jupiter Réalisez une étude de santé publique

---
+++ Réalisez une étude de santé publique&.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Projet 3  Réalisez une étude de santé publique
+
+# # Identifiez les grandes tendances
+
+# In[12]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[16]:
+
+
+from Réalisez une étude de santé publique import files
+import io
+uploaded = files.upload('fr_population.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+df_population = pd.read_csv('fr_population.csv')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
