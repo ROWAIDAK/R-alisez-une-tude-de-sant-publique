1e691d42798599604885160f5b5285d9c8195219
Rowaida KANAAN <rowaida.kanaan.dam@gmail.com>
2021-11-28 16:32:07 +0100

    Delete 1.sql

--- projet 3/FAO_2013_fr/SQL/1.sql
+++
@@ -1,5 +0,0 @@
-SELECT produit, AVG(autres_utilisations / dispo_int) AS ratio_autres_sur_dispo_interieure
-FROM equilibre_prod
-GROUP BY produit
-ORDER BY ratio_autres_sur_dispo_interieure DESC
-LIMIT 0,10
