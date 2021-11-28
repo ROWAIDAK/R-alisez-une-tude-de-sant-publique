db7b18c7e2339c295634d1d21224992230917b09
Rowaida KANAAN <rowaida.kanaan.dam@gmail.com>
2021-11-28 16:32:11 +0100

    Delete sécurité_alimentaire.db .sql

--- projet 3/FAO_2013_fr/SQL/sécurité_alimentaire.db .sql
+++
@@ -1,5 +0,0 @@
-SELECT sous_nutrition.pays, ((sous_nutrition.nb_personnes/population.population)*100) AS taux_sous_nutrition
-FROM sous_nutrition
-NATURAL JOIN population
-ORDER BY taux_sous_nutrition DESC
-LIMIT 0,10
